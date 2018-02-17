05fc0e35dcf93a153bb2695ccef5587f97131fd0
P.J. Ball <pjb208@mlsalt-cpu2.eng.cam.ac.uk>
2018-02-17 00:33:54 +0000

    Extensive changes: Added VAE_Train.py script to train from the command line, stole code from Dr.Shen Made the code compatible with tensorboard

--- VAE_NN.py
+++ VAE_NN.py
@@ -5,18 +5,21 @@ from torch.autograd import Variable
 from torch.nn import functional as F
 from torchvision import datasets, transforms
 from tqdm import tqdm   # Progress bar
+
+from tensorboardX import SummaryWriter
+
 #from torchvision.utils import save_image
 
 class VAE_Net(nn.Module):
     
     # MAIN VAE Class
 
-    def __init__(self):
+    def __init__(self, latent_size=20):
         super(VAE_Net, self).__init__()
 
         # define the encoder and decoder
 
-        self.latent = 20
+        self.latent = latent_size
 
         self.ei = nn.Linear(28 * 28 * 1, 500)
         self.em = nn.Linear(500, self.latent)
@@ -101,20 +104,29 @@ def get_data_loaders(b_size):
 
 
 
-def train(model, optimizer, train_loader, loss_func, epochs = 1, show_prog = 100):
+def train(model, optimizer, train_loader, loss_func, epochs = 1, show_prog = 100, summary = None):
 
     # stolen from a generic pytorch training implementation
     # TODO: Train on different data
     
+    if summary:
+        writer = SummaryWriter(summary)
+
+    b_size = float(train_loader.batch_size)
+    
     model.train()
     for i in tqdm(range(epochs)):
         for batch_idx, (data, _ ) in enumerate(train_loader):
+
+            n_iter = (i*len(train_loader))+batch_idx
             
             data = Variable(data, requires_grad = False).view(-1,784)  # NEED TO FLATTEN THE IMAGE FILE
             data = data.cuda()  # Make it GPU friendly
             optimizer.zero_grad()   # reset the optimzer so we don't have grad data from the previous batch
             output, mu, var = model(data)   # forward pass
             loss = loss_func(mu, var, data, output) # get the loss
+            if summary:
+                writer.add_scalar('ave loss/datapoint', loss.data[0]/b_size, n_iter)
             loss.backward() # back prop the loss
             optimizer.step()    # increment the optimizer based on the loss (a.k.a update params?)
             #print('Batch Training Loss is: %.6f' % loss[0])
@@ -122,3 +134,9 @@ def train(model, optimizer, train_loader, loss_func, epochs = 1, show_prog = 100
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     i, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.data[0]))
+                if summary:
+                    writer.add_image('real_image', data[1].view(-1,28,28), n_iter)
+                    a,_,_ = model(data[1].cuda())
+                    writer.add_image('reconstruction', a.view(-1,28,28), n_iter)
+                    b = model.decode(model.sample())
+                    writer.add_image('from_noise', b.view(-1,28,28), n_iter)
---
+++ train_vae.py
@@ -0,0 +1,81 @@
+import os
+import argparse
+import numpy as np
+
+from torch.autograd import Variable
+
+import VAE_NN
+from torchvision import datasets, transforms
+
+import time
+
+from torch.optim import Adam
+
+# TODO: Make this work with the dataloader that Prof. Shen created
+
+#from load_data import load_data
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    #parser.add_argument('--model', type=str, default='vae', choices=['vae'],
+    #                    help='type of variational autoencoder model (default: vae)')
+    #parser.add_argument('--dataset', type=str, default='mnist',
+    #                    choices=['mnist', 'frey_face', 'fashion_mnist'],
+    #                    help='dataset on which to train (default: mnist)\n' +
+   #                          'options: [mnist, frey_face, fashion_mnist, cifar10, cifar100]')
+
+    # TODO: input checks
+    # TODO: add ability to pass hyperparameter values as a .json file
+    # ISSUE: any way to add ability to specify encoder/decoder architectures?
+    # parser.add_argument('--hparams_file', type=str, default='./hparams.json',
+    #                     help='JSON file specifying the hyperparameters for training and record keeping')
+    parser.add_argument('--experiment_dir', type=str, default='./experiment',
+                        help='directory to which to output training summary and checkpoint files')
+
+    #parser.add_argument('--seed', type=int, default=123, help='seed for rng (default: 123)')
+    parser.add_argument('--num_epochs', type=int, default=100,
+                        help='number of training epochs (default: 100)')
+    parser.add_argument('--batch_size', type=int, default=100,
+                        help='number of samples per batch (default: 100)')
+    #parser.add_argument('--checkpoint_freq', type=int, default=100,
+    #                    help='frequency (in epochs) with which we save model checkpoints (default: 100)')
+
+    # also allow specification of optimizer to use?
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
+    parser.add_argument('--z_dim', type=int, default=20, help='dimensionality of latent variable')
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+    # output directories
+    # anything else that should be outputted/recorded? logs? example images?
+    checkpoint_dir = os.path.join(args.experiment_dir, "checkpoints")
+    # remove model saving for now
+    #checkpoint_path = os.path.join(checkpoint_dir, "model")
+    summary_dir = os.path.join(args.experiment_dir, "summaries")
+    if not os.path.exists(args.experiment_dir):
+        os.makedirs(args.experiment_dir)
+    #if not os.path.exists(checkpoint_dir):
+    #    os.makedirs(checkpoint_dir)
+    if not os.path.exists(summary_dir):
+        os.makedirs(summary_dir)
+
+    vae_n = VAE_NN.VAE_Net(args.z_dim)
+
+    # make it trainable on the GPU
+    vae_n.cuda()
+
+    optimizer = Adam(vae_n.parameters(),lr=args.lr)
+
+    train_data,_ = VAE_NN.get_data_loaders(b_size=args.batch_size)
+
+    t = time.time()
+
+    VAE_NN.train(vae_n,optimizer,train_data, VAE_NN.elbo_loss, epochs = args.num_epochs, summary = summary_dir)
+    t_e = time.time() - t
+    print('Seconds for %d epcohs: %d' % (ep,t_e))
